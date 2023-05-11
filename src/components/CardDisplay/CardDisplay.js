d70996d4906e5699efb52bbc1a18f94da01877f9
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-05-11 09:39:37 -0400

    Add semicolons and remove dummy data

--- src/components/CardDisplay/CardDisplay.js
+++ src/components/CardDisplay/CardDisplay.js
@@ -1,19 +1,13 @@
 import React from "react";
 import "./CardDisplay.css";
-import Card from "../Card/Card"
+import Card from "../Card/Card";
 
 const CardDisplay = () => {
   return (
     <div className="card-display">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {/* list of cards here */}
     </div>
   )
 }
 
-export default CardDisplay
+export default CardDisplay;
