bdaa01ee67ea222e6714ab43f4749e45f95a7b9e
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-05-11 07:35:52 -0400

    Add class names to card elements

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,9 +3,9 @@ import "./Card.css";
 
 const Card = ({ poster_path, title }) => {
   return (
-    <div>
-      <img src={poster_path}/>
-      <h2>{title}</h2>
+    <div className="card">
+      <img className="poster" src={poster_path}/>
+      <h2 className="title">{title}</h2>
     </div>
   )
 } 
