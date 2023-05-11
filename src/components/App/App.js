85bce5a6a54aa8321bfc4647306fb884ce46ffcf
Adriane Sutherland <asutherland91@gmail.com>
2023-05-11 13:20:55 -0700

    Add several minor changes for code uniformity, and new needed files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
+import CardDisplay from '../CardDisplay/CardDisplay';
 
 class App extends Component {
   constructor() {
@@ -8,10 +9,12 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        {/* <Header />
-        <CardDisplay /> */}
-      </div>
+      <main>
+        <div>
+          <Header />
+          <CardDisplay />
+        </div>
+      </main>
     )
   }
 }
