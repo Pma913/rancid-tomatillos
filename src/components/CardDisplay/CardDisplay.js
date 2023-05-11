4227e2333646b8f052b5e895d88136689acd0ea5
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-05-11 09:12:35 -0400

    Add base build out for CardDisplay component w/ imports and export default

---
+++ src/components/CardDisplay/CardDisplay.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./CardDisplay.css";
+import Card from "../Card/Card"
+
+const CardDisplay = () => {
+  return (
+    <div>
+      <Card />
+    </div>
+  )
+}
+
+export default CardDisplay
