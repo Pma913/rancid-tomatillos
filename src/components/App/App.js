cc66e342321291fdb7bef58ad0332dedecee4ae3
Adriane Sutherland <asutherland91@gmail.com>
2023-05-10 20:20:44 -0700

    Add new images for the header component, and setup/styling for the header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from '../Header/Header';
 
 class App extends Component {
   constructor() {
@@ -8,8 +9,8 @@ class App extends Component {
   render() {
     return (
         <div>
-          {/* <Header />
-          <CardDisplay /> */}
+          <Header />
+          {/* <CardDisplay /> */}
         </div>
     )
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+import title from "../../Images/Rancid-Tomatillos-5-10-2023.png"
+import logo from "../../Images/tomatillos.png"
+
+const Header = () => {
+  return (
+    <header>
+      <img className="title" src={title} alt="Rancid tomatillos text" />
+      <img className="logo" src={logo} alt=" three cartoon tomatillos"/>
+    </header>
+  )
+}
+
+export default Header;
