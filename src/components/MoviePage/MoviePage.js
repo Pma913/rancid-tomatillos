75955e7f2be88bd6089cef7d8a924ffc749aa0dc
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-05-12 09:36:48 -0400

    Enable data passed through props to be displayed within MoviePage component

--- src/components/MoviePage/MoviePage.js
+++ src/components/MoviePage/MoviePage.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
-const MoviePage = () => {
+const MoviePage = ({ props }) => {
+  console.log('tagline', props)
   return (
     <div>
       <div className="top">
-        <img className="back-img" src={back-img}/>
-        <h2 className="movie-tag">{tagline}</h2>
+        <img className="back-img" src={props.backdrop_path}/>
+        <h2 className="movie-tag">{props.tagline}</h2>
         <button className="home-btn">HOME</button>
       </div>
       <div className="left-side">
-        <h2 className="movie-title">{title}</h2>
-        <p className="overview">{overview}</p>
+        <h2 className="movie-title">{props.title}</h2>
+        <p className="overview">{props.overview}</p>
       </div>
       <div className="righ-side">
-        <img className="back-img" src={post-img}/>
+        <img className="back-img" src={props.poster_path}/>
       </div>
     </div>
   )
