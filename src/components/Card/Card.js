fde785e9b1ad9aa3a73bab3143eca7beb3600349
Adriane Sutherland <asutherland91@gmail.com>
2023-05-11 22:31:33 -0700

    Add the ability to click on the cards and log the details

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./Card.css";
+import { getMovies } from '../../utilities/api-calls'
 
 const Card = ({ img, title, id }) => {
   return (
-    <div id={id} className="card">
+    <div id={id} className="card" onClick={() => {
+      getMovies(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+      .then(data => console.log(data.movie))
+      .catch(err => console.log(err));
+    }}>
       <img className="poster" src={img}/>
       <h2 className="title">{title}</h2>
     </div>
   )
 } 
 
-export default Card
+export default Card;
