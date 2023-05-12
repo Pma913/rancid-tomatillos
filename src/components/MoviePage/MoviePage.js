6f278140a9d9979610b915ecff615e4f8a879a77
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-05-12 10:50:10 -0400

    Add more sectioning with div

--- src/components/MoviePage/MoviePage.js
+++ src/components/MoviePage/MoviePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './MoviePage.css';
 
 const MoviePage = ({ props, clearState }) => {
   console.log('tagline', clearState)
@@ -9,12 +10,14 @@ const MoviePage = ({ props, clearState }) => {
         <h2 className="movie-tag">{props.tagline}</h2>
         <button className="home-btn" onClick={() => clearState()}>HOME</button>
       </div>
-      <div className="left-side">
-        <h2 className="movie-title">{props.title}</h2>
-        <p className="overview">{props.overview}</p>
-      </div>
-      <div className="righ-side">
-        <img className="back-img" src={props.poster_path}/>
+      <div className="movie-container">
+        <div className="left-side">
+          <h2 className="movie-title">{props.title}</h2>
+          <p className="overview">{props.overview}</p>
+        </div>
+        <div className="right-side">
+          <img className="post-img" src={props.poster_path}/>
+        </div>
       </div>
     </div>
   )
