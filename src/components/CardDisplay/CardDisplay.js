4995065b9d21e76a387e1a70969ecfd4150824d4
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-05-17 18:18:47 -0400

    Add navlink component to cards and remove the clearstate method

--- src/components/CardDisplay/CardDisplay.js
+++ src/components/CardDisplay/CardDisplay.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./CardDisplay.css";
 import Card from "../Card/Card";
+import { NavLink } from "react-router-dom";
 
-const CardDisplay = ({ movies, setMovie }) => {
+const CardDisplay = ({ movies }) => {
   const movieCards = movies.map(movie => {
     return (
-      <Card
-      key={movie.id} 
-      id={movie.id} 
-      img={movie.poster_path} 
-      title={movie.title} 
-      setMovie={setMovie} />
-      )
+      <NavLink key={movie.id} to={`/movie/${movie.id}`} >
+        <Card 
+        id={movie.id} 
+        img={movie.poster_path} 
+        title={movie.title} 
+        />
+      </NavLink>
+    )
   })
   return (
     <div className="card-display">
