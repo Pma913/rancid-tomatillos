6ee000cb889f3f851159304beda9516d327eb5b1
Adriane Sutherland <asutherland91@gmail.com>
2023-05-19 10:49:26 -0700

    Add updated CSS styling for the movie page

--- src/components/MoviePage/MoviePage.js
+++ src/components/MoviePage/MoviePage.js
@@ -35,23 +35,25 @@ class MoviePage extends Component {
           <div className="top">
             <Link to="/" className="home-btn"><i className="arrow left"></i></Link>
             <h2 className="movie-tag">{movie.tagline}</h2>
-            <div className="movie-player">
-
-              {this.state.video ? <ReactPlayer controls={true} url={`https://www.youtube.com/watch?v=${this.state.video}`} /> :
-                <div>
-                  <h2>Sorry, there currently is no video available for this movie</h2>
-                  <p>Try searching <a href="https://www.youtube.com/">YouTube</a> for what you are looking for</p>
-                </div>}
-
-            </div>
           </div>
           <div className="movie-container">
             <div className="left-side">
               <h2 className="movie-title">{movie.title}</h2>
               <p className="overview">{movie.overview}</p>
+              <div className="movie-player">
+                {this.state.video ? <ReactPlayer controls={true} url={`https://www.youtube.com/watch?v=${this.state.video}`} /> :
+                  <div className="no-movies-wrapper">
+                    <h2 className="no-video">Sorry, there currently are no videos available for this movie</h2>
+                    <p className="no-video2">Try searching <a href="https://www.youtube.com/">YouTube</a> for what you are looking for</p>
+                  </div>
+                }
+            </div>
             </div>
             <div className="right-side">
               <img className="post-img" src={movie.poster_path}/>
+              <p className="extra-details">Movie Runtime: {movie.runtime} minutes</p>
+              {movie.genres && <p className="extra-details"> Genre: {movie.genres.join(", ")}</p>}
+              <p className="extra-details">Average Rating: {movie.average_rating}</p>
             </div>
           </div>
         </div>
