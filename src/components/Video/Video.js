c6a48f4744b7670bb2edd494362b561751f2f968
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-05-20 14:07:40 -0400

    Initial commit

---
+++ src/components/Video/Video.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+const Video = ( { allMovies } ) => {
+  if (allMovies) {
+    const movies = allMovies.map(vid => {
+      return (
+        <div key={vid.key} >
+          <ReactPlayer controls={true} url={`https://www.youtube.com/watch?v=${vid.key}`} />
+        </div>
+      )
+    });
+    return (
+      <div>
+        {movies}
+      </div>
+    )
+  } else {
+    return (
+      <div className="no-movies-wrapper">
+        <h2 className="no-video">Sorry, there currently are no videos available for this movie</h2>
+        <p className="no-video2">Try searching <a href="https://www.youtube.com/">YouTube</a> for what you are looking for</p>
+      </div>
+    )
+  }
+}
+
+export default Video;
